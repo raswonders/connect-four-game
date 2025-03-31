b4c8b5c9abd9cb9c70876bb379008e9bb54654b4
Rastislav Hepner <rastislav.hepner@gmail.com>
2025-03-31 18:38:29 +0200

    add isGameWon function

--- src/components/ui/GameGrid.tsx
+++ src/components/ui/GameGrid.tsx
@@ -1,22 +1,38 @@
-import { useGrid } from "../useGrid";
+import { useEffect, useState } from "react";
+import { useGrid, Player } from "../useGrid";
+
 const COLS = 7;
 const ROWS = 6;
 
 export function GameGrid() {
-  const { addDisc, getDiscs, playerOne } = useGrid(ROWS, COLS);
+  const { addDisc, getDiscs, playerOne, playerTwo, isGameWon } =
+    useGrid(ROWS, COLS);
+  const [player, setPlayer] = useState<Player>(playerTwo);
+  useEffect(() => {
+    console.log(`game is ${isGameWon(playerOne) ? "won!" : "not won yet..."}`);
+  });
 
   return (
-    <div className="grid grid-cols-7 grid-rows-6 gap-4">
-      {getDiscs().map((disc, index) => (
-        <div
-          onClick={() => {
-            addDisc(index % COLS, playerOne);
-          }}
-          className={`h-10 border border-neutral-300 ${
-            disc ? (disc === playerOne ? "bg-red-400" : "bg-yellow-400") : ""
-          } column-${index % COLS}`}
-        ></div>
-      ))}
-    </div>
+    <>
+      <div className="grid grid-cols-7 grid-rows-6 gap-4">
+        {getDiscs().map((disc, index) => (
+          <div
+            onClick={() => {
+              addDisc(index % COLS, player);
+            }}
+            className={`h-10 border border-neutral-300 ${
+              disc ? (disc === playerOne ? "bg-red-400" : "bg-yellow-400") : ""
+            } column-${index % COLS}`}
+          ></div>
+        ))}
+      </div>
+      <select
+        value={player}
+        onChange={(event) => setPlayer(parseInt(event.target.value) as Player)}
+      >
+        <option value="1">Player One</option>
+        <option value="2">Player Two</option>
+      </select>
+    </>
   );
 }
--- src/components/useGrid.tsx
+++ src/components/useGrid.tsx
@@ -5,7 +5,7 @@ const playerOne = 1 as const;
 const playerTwo = 2 as const;
 const playerEmpty = 0 as const;
 
-type Player = -1 | 0 | 1 | 2;
+export type Player = -1 | 0 | 1 | 2;
 
 export function useGrid(rows: number, cols: number) {
   const [grid, setGrid] = useState<Player[][]>(
@@ -31,16 +31,72 @@ export function useGrid(rows: number, cols: number) {
     return grid.flat();
   }
 
-  return { addDisc, getDiscs, playerOne };
-}
+  function isGameWon(player: Player): boolean {
+    for (let row = 0; row < rows; row++) {
+      for (let col = 0; col < cols; col++) {
+        if (grid[row][col] === player) {
+          return victoryCheck(player, row, col);
+        }
+      }
+    }
+    return false;
+  }
 
-export function checkWin() {
-  // TODO
-  // Level 0
-  // for each cell (do 4 dir checks)
-  //  right check
-  //  down check
-  //  down-right check
-  //  down-left check
-  // Level 1 Exclude double checking cells which have no chance
+  function victoryCheck(player: Player, row: number, col: number): boolean {
+    return (
+      rightCheck(player, row, col) ||
+      downCheck(player, row, col) ||
+      downRightCheck(player, row, col) ||
+      downLeftCheck(player, row, col)
+    );
+  }
+  function rightCheck(player: Player, row: number, col: number) {
+    for (let c = col + 1; c < col + 4; c++) {
+      if (c >= cols) {
+        return false;
+      }
+      if (grid[row][c] !== player) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  function downCheck(player: Player, row: number, col: number) {
+    for (let r = row + 1; r < row + 4; r++) {
+      if (r >= rows) {
+        return false;
+      }
+      if (grid[r][col] !== player) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  function downRightCheck(player: Player, row: number, col: number) {
+    for (let r = row + 1, c = col + 1; r < row + 4 && c < col + 4; r++, c++) {
+      if (r >= rows || c >= cols) {
+        return false;
+      }
+      if (grid[r][c] !== player) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  function downLeftCheck(player: Player, row: number, col: number) {
+    for (let r = row + 1, c = col - 1; r < row + 4 && c >= col - 4; r++, c--) {
+      if (r >= rows || c < 0) {
+        return false;
+      }
+      if (grid[r][c] !== player) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  return { addDisc, getDiscs, playerOne, playerTwo, isGameWon };
 }
