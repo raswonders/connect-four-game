8ac7bbcf426705fd2cfd319145373faaf5d48486
Rastislav Hepner <rastislav.hepner@gmail.com>
2025-04-02 21:04:29 +0200

    style navbar

--- src/components/ui/Navbar.tsx
+++ src/components/ui/Navbar.tsx
@@ -1,3 +1,21 @@
+import ovalYellow from "../../assets/oval-yellow.svg";
+import ovalRed from "../../assets/oval-red.svg";
+
 export function Navbar() {
-  return <h1>navbar</h1>;
+  return (
+    <div className="flex justify-between items-center">
+      <button className="rounded-full bg-dark-purple py-2 px-5 uppercase font-bold">
+        Menu
+      </button>
+      <div className="grid grid-cols-2 gap-1">
+        <img src={ovalRed} alt="" />
+        <img src={ovalYellow} alt="" />
+        <img src={ovalYellow} alt="" />
+        <img src={ovalRed} alt="" />
+      </div>
+      <button className="rounded-full bg-dark-purple py-2 px-5 uppercase font-bold">
+        Restart
+      </button>
+    </div>
+  );
 }
