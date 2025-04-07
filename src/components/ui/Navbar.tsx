b6a8cfacbe3b24d30980a3499ed05faa514b1963
Rastislav Hepner <rastislav.hepner@gmail.com>
2025-04-07 20:52:17 +0200

    shrink the logo on mobile

--- src/components/ui/Navbar.tsx
+++ src/components/ui/Navbar.tsx
@@ -10,8 +10,8 @@ export function Navbar({ className }: Props) {
     <div className={`flex justify-between items-center ${className}`}>
       <button className="rounded-full bg-dark-purple py-2 px-5 uppercase font-bold">
         Menu
-      </button>
-      <div className="grid grid-cols-2 gap-1">
+      </button>a
+      <div className="w-10 grid grid-cols-2 gap-0.5">
         <img src={ovalRed} alt="" />
         <img src={ovalYellow} alt="" />
         <img src={ovalYellow} alt="" />
