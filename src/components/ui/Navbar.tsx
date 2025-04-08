a2a736f76c064d32777e0b47a582dd8d2685fe51
Rastislav Hepner <rastislav.hepner@gmail.com>
2025-04-08 20:48:53 +0200

    navbar: make buttons similar in size

--- src/components/ui/Navbar.tsx
+++ src/components/ui/Navbar.tsx
@@ -8,16 +8,16 @@ interface Props {
 export function Navbar({ className }: Props) {
   return (
     <div className={`flex justify-between items-center ${className}`}>
-      <button className="rounded-full bg-dark-purple py-2 px-5 uppercase font-bold">
+      <button className="min-w-28 rounded-full bg-dark-purple p-2 uppercase font-bold">
         Menu
-      </button>a
+      </button>
       <div className="w-10 grid grid-cols-2 gap-0.5">
         <img src={ovalRed} alt="" />
         <img src={ovalYellow} alt="" />
         <img src={ovalYellow} alt="" />
         <img src={ovalRed} alt="" />
       </div>
-      <button className="rounded-full bg-dark-purple py-2 px-5 uppercase font-bold">
+      <button className="min-w-28 rounded-full bg-dark-purple p-2 uppercase font-bold">
         Restart
       </button>
     </div>
